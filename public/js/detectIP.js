0226a0ee11e6497d9e0569c9c8e962c1f97f13c6
DangHoangThien1995 <taikhoancuatoi2009@gmail.com>
2017-05-29 21:42:31 +0700

    fix-detectip

--- public/js/detectIP.js
+++ public/js/detectIP.js
@@ -3,4 +3,8 @@ $.getJSON('https://ipinfo.io/geo', function(response) {
     if (response.country == 'VN') {
         window.location.href = '/vn';
     }
+    else {
+    	window.location.href = '/en';
+    }
+    console.log(response.ip);
 });
